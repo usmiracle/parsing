ac16e45ed9b4e7475149bcd0cb4c23427c464cd7
Sulabh Katila <sulabhkatila@gmail.com>
2025-07-14 17:20:07 -0400

    Update Environment.py

--- Environment.py
+++ Environment.py
@@ -12,13 +12,13 @@ class Type:
 class Environment:
     def __init__(self, enclosing: Environment | None = None):
         self.enclosing: Environment | None = enclosing # enclosing is the globals
-        self.values: dict[str, str] = {}
+        self.values: dict[str, Type] = {}
 
-    def define(self, name: str, value: str):
+    def define(self, name: str, value: Type):
         """Define a new variable in the current environment."""
         self.values[name] = value
 
-    def get(self, name: str) -> str | None:
+    def get(self, name: str) -> Type | None:
         """Get a variable from the current environment or parent."""
         if name in self.values:
             return self.values[name]
@@ -26,7 +26,7 @@ class Environment:
             return self.enclosing.get(name)
         return None
 
-    def assign(self, name: str, value: str) -> bool:
+    def assign(self, name: str, value: Type) -> bool:
         """Assign an existing variable, searching in enclosing environments."""
         if name in self.values:
             self.values[name] = value
